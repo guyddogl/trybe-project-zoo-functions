7b3700106a156805678d4749d1a85e2bdb320d7c
Guyddo <guyddogl@gmail.com>
2022-06-29 19:41:37 -0300

    Testa se handlerElephants é uma função

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,8 @@
+const { TestScheduler } = require('jest');
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('Verifica se handlerElephants é uma função', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+});
