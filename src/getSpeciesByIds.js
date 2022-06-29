0c1b808bd63bf7074c9992cf9d6a4aea2a0f3a79
Guyddo <guyddogl@gmail.com>
2022-06-29 15:42:10 -0300

    Correções de lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,12 +2,7 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   const { species } = data;
-  return ids.map((id) => {
-    return species.find((specie) => {
-      return specie.id === id;
-    });
-  });
-
+  return ids.map((id) => species.find((specie) => specie.id === id));
 }
 
 module.exports = getSpeciesByIds;
