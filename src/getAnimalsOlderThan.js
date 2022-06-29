f10a45d9ae2323651eb1fa4640e799d729530ce9
Guyddo <guyddogl@gmail.com>
2022-06-29 15:55:11 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  const animalSpecie = species.find((specie) => specie.name === animal);
+  const animalResidents = animalSpecie.residents;
+  return animalResidents.every((element) => element.age >= age);
 }
+
+getAnimalsOlderThan('lions', '7');
 
 module.exports = getAnimalsOlderThan;
