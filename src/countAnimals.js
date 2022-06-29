2379f91852534a6fc6f2c94aa5439838cce88542
Guyddo <guyddogl@gmail.com>
2022-06-29 19:34:46 -0300

    Correções de lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,23 +2,16 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-const allAnimals = () => {
-  return species.reduce((acc, specie) => {
-    acc[specie.name] = specie.residents.length;
-    return acc;
-  }, {});
-};
+const allAnimals = () => species.reduce((acc, specie) => {
+  acc[specie.name] = specie.residents.length;
+  return acc;
+}, {});
 
 function countAnimals(animal) {
   if (!animal) return allAnimals();
-  const residents = species.find((specie) => specie.name === animal.specie).residents;
+  const { residents } = species.find((specie) => specie.name === animal.specie);
   if (!animal.sex) return residents.length;
   return residents.filter((element) => element.sex === animal.sex).length;
-  
 }
-
-// console.log(countAnimals());
-// console.log(countAnimals({ specie: 'giraffes' }));
-console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
