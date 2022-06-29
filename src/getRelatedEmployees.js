285b5f791485d22d3d7f954005db6c455f36a4db
Guyddo <guyddogl@gmail.com>
2022-06-29 16:43:23 -0300

    Requisito 4 - função isManager

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const { employees } = data;
+  return employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw  new  Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return console.log(managerId);
 }
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
