3209e306994eae49e24e8a5e878fe3f8423dcffc
Guyddo <guyddogl@gmail.com>
2022-06-29 16:24:22 -0300

    Correções de lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,9 +3,8 @@ const data = require('../data/zoo_data');
 function getEmployeeByName(employeeName) {
   const { employees } = data;
   if (!employeeName) return {};
-  return employees.find((element) => element.firstName === employeeName || element.lastName === employeeName);
+  return employees.find((element) =>
+    element.firstName === employeeName || element.lastName === employeeName);
 }
-
-console.log(getEmployeeByName('Nigel'));
 
 module.exports = getEmployeeByName;
