3c80acfab5ccf52233c158b928ce37b78b617465
Guyddo <guyddogl@gmail.com>
2022-06-29 17:59:23 -0300

    Refatorando função getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -10,12 +10,8 @@ function getRelatedEmployees(managerId) {
   if (!isManager(managerId)) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
-  // const responsibleFor = employees.find((manager) => manager.id === managerId).responsibleFor;
-  // console.log(responsibleFor);
-  const responsibleFor2 = employees.filter((employee) => employee.managers.includes(managerId));
-  return responsibleFor2.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  return employees.filter((employee) => employee.managers.includes(managerId))
+ .map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
-
-// console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
