2be87b4f9d0a7cf4d7f669072d05551d032aa5d8
Guyddo <guyddogl@gmail.com>
2022-06-29 19:46:23 -0300

    Testa se a função handlerElephants retorna undefined ao receber undefined como parâmetro

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -5,4 +5,7 @@ describe('Testes da função HandlerElephants', () => {
   test('Verifica se handlerElephants é uma função', () => {
     expect(typeof handlerElephants).toBe('function');
   });
+  test('Testa se ao receber undefined como parâmetro a função handlerElephants retorna undefined', () => {
+    expect(handlerElephants(undefined)).toBeUndefined();
+  });
 });
