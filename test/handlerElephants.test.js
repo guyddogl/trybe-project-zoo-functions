aa782076c36c8a7b19b295de39cfd73040dac888
Guyddo <guyddogl@gmail.com>
2022-06-29 20:04:04 -0300

    Verifica se a função handlerElephants retorna a localização dos elefantes ao receber location como parâmetro

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -5,17 +5,20 @@ describe('Testes da função HandlerElephants', () => {
   test('Verifica se handlerElephants é uma função', () => {
     expect(typeof handlerElephants).toBe('function');
   });
-  test('Testa se ao receber undefined como parâmetro a função handlerElephants retorna undefined', () => {
+  test('Verifica se ao receber undefined como parâmetro a função handlerElephants retorna undefined', () => {
     expect(handlerElephants(undefined)).toBeUndefined();
   });
-  test('Testa se ao receber count como parâmetro a função handlerElephants retorna 4', () => {
+  test('Verifica se ao receber count como parâmetro a função handlerElephants retorna 4', () => {
     expect(handlerElephants('count')).toBe(4);
   });
-  test('Testa se ao receber names como parâmetro a função handlerElephants retorna um array com os nomes dos elefantes', () => {
+  test('Verifica se ao receber names como parâmetro a função handlerElephants retorna um array com os nomes dos elefantes', () => {
     const elephantsNames = ['Ilana', 'Orval', 'Bea', 'Jefferson']
     expect(handlerElephants('names')).toEqual(elephantsNames);
   });
-  test('Testa se ao receber averageAge como parâmetro a função handlerElephants retorna a média de idade dos elefantes', () => {
+  test('Verifica se ao receber averageAge como parâmetro a função handlerElephants retorna a média de idade dos elefantes', () => {
     expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  test('Verifica se ao receber location como parâmetro a função handlerElephants retorna a a localização dos elefantes', () => {
+    expect(handlerElephants('location')).toBe('NW');
   });
 });
