73574997356f9ca68928a7e980038c545c00e566
Guyddo <guyddogl@gmail.com>
2022-06-29 15:38:56 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  return ids.map((id) => {
+    return species.find((specie) => {
+      return specie.id === id;
+    });
+  });
+
 }
 
 module.exports = getSpeciesByIds;
