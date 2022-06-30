82186f5be81bbeed354262b77b6c33e67ac17ced
Guyddo <guyddogl@gmail.com>
2022-06-30 08:40:13 -0300

    Verifica se getOpeningHours retorna um objeto com todos os horários de funcionamento caso não receba parâmetro

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -4,4 +4,16 @@ describe('Testes da função getOpeningHours', () => {
   test('Verifica se getOpeningHours é uma função', () => {
     expect(typeof getOpeningHours).toBe('function');
   });
+  test('Verifica se getOpeningHours retorna um objeto com todos os horários de funcionamento caso não receba parâmetro', () => {
+    const noParam = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    }
+    expect(getOpeningHours()).toEqual(noParam);
+  });
 });
