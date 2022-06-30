903c2226fee1720c02b3c321ce31d426298507af
Guyddo <guyddogl@gmail.com>
2022-06-29 23:55:34 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    child: entrants.filter((child) => child.age < 18).length,
+    adult: entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length,
+    senior: entrants.filter((senior) => senior.age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || entrants.length === 0 || Object.keys(entrants).length === 0) return 0;
+  const peopleGroup = countEntrants(entrants);
+  const { child, adult, senior } = prices;
+  const totalChild = child * peopleGroup.child;
+  const totalAdult = adult * peopleGroup.adult;
+  const totalSenior = senior * peopleGroup.senior;
+  return totalChild + totalAdult + totalSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
