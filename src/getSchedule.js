8e0b280baaffc797a7d32a6349038f87477f1dfc
Guyddo <guyddogl@gmail.com>
2022-06-30 10:08:29 -0300

    Requisito 9: Cria a funçãi isValidAnimal

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,15 +1,15 @@
 const data = require('../data/zoo_data');
 
 const { species } = data;
-const weekDays = ['Tuesday', 'Wednesday', 'Sunday', 'Saturday'];
+// const weekDays = ['Tuesday', 'Wednesday', 'Sunday', 'Saturday'];
 
 const isValidAnimal = (animal) => species.some((animalName) => animalName.name === animal);
 
 function getSchedule(scheduleTarget) {
-  
+  if (isValidAnimal) return species.find((animal) => animal.name === scheduleTarget).availability;
 }
 
 // console.log(getSchedule('lions'));
-console.log(getSchedule());
+// console.log(getSchedule());
 
 module.exports = getSchedule;
