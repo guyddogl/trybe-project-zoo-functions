d6b8aeca237144b0a4150f1b4ae869b6c6cc3d70
Guyddo <guyddogl@gmail.com>
2022-06-30 14:50:52 -0300

    Requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -5,35 +5,48 @@ const { hours } = data;
 const weekDays = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
 
 const isValidAnimal = (animal) => species.some((animalName) => animalName.name === animal);
+const isValidDay = (day) => weekDays.some((weekDay) => weekDay === day);
+const isValidParam = (param) => param !== undefined;
 
 const getOpeningHours = (day) => {
-  const officeHour = {};
-  if (day === 'Monday') {
-    officeHour.officeHour = 'CLOSED';
-    return officeHour;
-  }
+  if (day === 'Monday') return 'CLOSED';
   const { open, close } = hours[day];
-  officeHour.officeHour = `Open from ${open}am until ${close}pm`;
-  return officeHour;
+  return `Open from ${open}am until ${close}pm`;
+};
+
+const getAnimalsExhibition = (day) => {
+  if (day === 'Monday') return 'The zoo will be closed!';
+  return species.filter((animal) => animal.availability.includes(day)).map((e) => e.name);
 };
 
 const getOpeningDays = () => {
   const schedule = {};
   weekDays.forEach((day) => {
-    schedule[day] = getOpeningHours(day);
+    const objSchedule = {
+      officeHour: getOpeningHours(day),
+      exhibition: getAnimalsExhibition(day),
+    };
+    schedule[day] = objSchedule;
   });
   return schedule;
 };
-console.log(getOpeningDays());
+
+const getScheduleDay = (day) => {
+  const scheduleDay = {};
+  scheduleDay[day] = {
+    officeHour: getOpeningHours(day),
+    exhibition: getAnimalsExhibition(day),
+  };
+  return scheduleDay;
+};
 
 function getSchedule(scheduleTarget) {
-  if (!scheduleTarget) {
-    return console.log(weekDays);
+  if (isValidAnimal(scheduleTarget)) {
+    return species.find((e) => e.name === scheduleTarget).availability;
   }
-  if (isValidAnimal) return species.find((animal) => animal.name === scheduleTarget).availability;
+  if (isValidDay(scheduleTarget)) return getScheduleDay(scheduleTarget);
+  if (!isValidParam(scheduleTarget)) return getOpeningDays();
+  return getOpeningDays();
 }
-
-// console.log(getSchedule('penguins'));
-// console.log(getSchedule());
 
 module.exports = getSchedule;
